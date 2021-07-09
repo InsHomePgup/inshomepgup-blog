c5d4af08fa8189d15121a8be8560c8039eb5d202
InsHomePgup <99671204@qq.com>
2021-07-09 17:23:22 +0800

    my blog

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,71 @@
+module.exports = {
+  title: "test blog",
+  description: 'It just a test.',
+  dest: 'example/docs/public',
+  head: [
+    ['link', { rel: 'icon', href: '/favicon.ico' }],
+    ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }]
+  ],
+  // theme: 'reco',
+  theme: 'reco',
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/', icon: 'reco-home' },
+      { text: 'TimeLine', link: '/timeLine/', icon: 'reco-date' },
+      { text: 'Contact',
+        icon: 'reco-message',
+        items: [
+          { text: 'NPM', link: 'https://www.npmjs.com/~reco_luan', icon: 'reco-npm' },
+          { text: 'GitHub', link: 'https://github.com/recoluan', icon: 'reco-github' },
+        ]
+      }
+    ],
+    type: 'blog',
+    // 博客设置
+    blogConfig: {
+      category: {
+        location: 2, // 在导航栏菜单中所占的位置，默认2
+        text: 'Category' // 默认 “分类”
+      },
+      tag: {
+        location: 3, // 在导航栏菜单中所占的位置，默认3
+        text: 'Tag' // 默认 “标签”
+      }
+    },
+    logo: '/head.png',
+    // 搜索设置
+    search: true,
+    searchMaxSuggestions: 10,
+    // 自动形成侧边导航
+    sidebar: 'auto',
+    // 最后更新时间
+    lastUpdated: 'Last Updated',
+    // 作者
+    author: 'PrtSc',
+    // 备案号
+    record: '备案号',
+    // 项目开始时间
+    startYear: '2017'
+    /**
+     * 密钥 (if your blog is private)
+     */
+
+    // keyPage: {
+    //   keys: ['your password'],
+    //   color: '#42b983',
+    //   lineColor: '#42b983'
+    // },
+
+    /**
+     * valine 设置 (if you need valine comment )
+     */
+
+    // valineConfig: {
+    //   appId: '...',// your appId
+    //   appKey: '...', // your appKey
+    // }
+  },
+  markdown: {
+    lineNumbers: true
+  }
+}
